1483ee5ba1872624729b74b96b451b24da97c322
mortimr <iulian@rotaru.fr>
2023-06-16 15:39:35 +0200

    pgeth

--- build/ci.go
+++ build/ci.go
@@ -202,6 +202,9 @@ func doInstall(cmdline []string) {
 		buildTags = append(buildTags, "ckzg")
 	}
 
+	// PGETH: ADD PUREGO BUILD TAG
+	buildTags = append(buildTags, "purego")
+
 	// Configure the build.
 	gobuild := tc.Go("build", buildFlags(env, *staticlink, buildTags)...)
 
